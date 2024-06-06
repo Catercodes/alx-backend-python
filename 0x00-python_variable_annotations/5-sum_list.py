6cc9ea67d9d713ae5d484d4370ec04e359a39709
Catercodes <chrisadamsadewuni@gmail.com>
2024-06-07 00:37:04 +0100

    sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""sum of list"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    args: input_list
+    return: sum of list
+    """
+    return sum(input_list)
