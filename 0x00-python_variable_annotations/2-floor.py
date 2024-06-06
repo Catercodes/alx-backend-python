7287fa05a21520ef130f9aa58a3d3430d8cb8758
Catercodes <chrisadamsadewuni@gmail.com>
2024-06-06 23:21:36 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"python environment"
+
+
+import math
+"Importing the math module"
+def floor(n : float) -> int:
+    "function that returns the floor of an int"
+    return math.floor(n)
