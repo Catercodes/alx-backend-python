c9866d58f291d5d9df9aa05c8ac6735f2ff4ebbf
Catercodes <chrisadamsadewuni@gmail.com>
2024-06-07 16:34:17 +0100

    Complex types - mixed list

--- 0x00-python_variable_annotations/6-main.py
+++ 0x00-python_variable_annotations/6-main.py
@@ -4,6 +4,8 @@ sum_mixed_list = __import__('6-sum_mixed_list').sum_mixed_list
 
 print(sum_mixed_list.__annotations__)
 mixed = [5, 4, 3.14, 666, 0.99]
+#chris = sum_mixed_list([1.11, 2.22, 3.33, 4, 5, 6, -666])
 ans = sum_mixed_list(mixed)
 print(ans == sum(mixed))
+#print(chris)
 print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int,float]]) -> float:
 
     "function of  mixed list"
     return sum(mxd_list)
